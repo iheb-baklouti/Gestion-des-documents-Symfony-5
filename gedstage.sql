319166ad04ebbd473c244db7916fd896f3e96c9e
iheb-baklouti <73111630+iheb-baklouti@users.noreply.github.com>
2022-01-26 14:09:36 +0100

    Add files via upload

---
+++ gedstage.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 26 jan. 2022 à 14:09
+-- Version du serveur : 10.4.20-MariaDB
+-- Version de PHP : 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `gedstage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+-- Erreur de lecture de structure pour la table gedstage.doctrine_migration_versions : #1932 - Table 'gedstage.doctrine_migration_versions' doesn't exist in engine
+-- Erreur de lecture des données pour la table gedstage.doctrine_migration_versions : #1064 - Erreur de syntaxe près de 'FROM `gedstage`.`doctrine_migration_versions`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fichier`
+--
+-- Erreur de lecture de structure pour la table gedstage.fichier : #1932 - Table 'gedstage.fichier' doesn't exist in engine
+-- Erreur de lecture des données pour la table gedstage.fichier : #1064 - Erreur de syntaxe près de 'FROM `gedstage`.`fichier`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fichier_user`
+--
+-- Erreur de lecture de structure pour la table gedstage.fichier_user : #1932 - Table 'gedstage.fichier_user' doesn't exist in engine
+-- Erreur de lecture des données pour la table gedstage.fichier_user : #1064 - Erreur de syntaxe près de 'FROM `gedstage`.`fichier_user`' à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+-- Erreur de lecture de structure pour la table gedstage.user : #1932 - Table 'gedstage.user' doesn't exist in engine
+-- Erreur de lecture des données pour la table gedstage.user : #1064 - Erreur de syntaxe près de 'FROM `gedstage`.`user`' à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
